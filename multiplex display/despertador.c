26e95998b1e7e2e6b2908291de422c8fc738b836
tui <70031851+tuisapo@users.noreply.github.com>
2022-11-14 14:46:10 -0300

    Create despertador.c

---
+++ multiplex display/despertador.c
@@ -0,0 +1,16 @@
+/*
+  fazer uma variavel global tipo
+  horario do cronometro ja definido = 0;
+  
+  nos timers, fazer um if:
+  
+  if ja definido: funcionamento padrao do relogio
+  if nao definido: funcionamento para definir
+  
+  da pra fazer 2 whiles tambem, um pra definir e outro pra contar/despertar
+  
+  quando verificar que o tempo é igual ao que eu queria, da pra desativar as interrupcoes dos timers
+  
+  if((minuto1 > 3 && minuto2 > 5) || hora > 4), por exemplo, se eu definir 45 minutos
+  
+*/
